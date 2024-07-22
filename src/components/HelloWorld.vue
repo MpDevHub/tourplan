15b825b64cd2719ca0d8dd51eca8e5d27679f2ff
MpDevHub <admin1@jembo.com>
2024-07-22 14:36:59 +0700

    Update HelloWorld.vue speed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,7 +29,7 @@ function playAudio(country) {
 
   audioPlayer.value.load();
   audioPlayer.value.play();
-  audioPlayer.value.playbackRate = 1.25;
+  // audioPlayer.value.playbackRate = 1.25;
 }
 
 function playAudioj2(country) {
@@ -48,7 +48,7 @@ function playAudioj2(country) {
 
   audioPlayer1.value.load();
   audioPlayer1.value.play();
-  audioPlayer1.value.playbackRate = 1.25;
+  // audioPlayer1.value.playbackRate = 1.25;
 }
 
 function playAudioj3(country) {
@@ -67,7 +67,7 @@ function playAudioj3(country) {
 
   audioPlayer2.value.load();
   audioPlayer2.value.play();
-  audioPlayer2.value.playbackRate = 1.25;
+  // audioPlayer2.value.playbackRate = 1.25;
 }
 
 function playAudioj4(country) {
@@ -86,7 +86,7 @@ function playAudioj4(country) {
 
   audioPlayer3.value.load();
   audioPlayer3.value.play();
-  audioPlayer3.value.playbackRate = 1.25;
+  // audioPlayer3.value.playbackRate = 1.25;
 }
 </script>
 
@@ -98,7 +98,7 @@ function playAudioj4(country) {
     <div class="flex-none">
       <ul class="menu menu-horizontal px-9">
         <li>
-          <details>
+          
             <label class="swap swap-rotate">
               <!-- this hidden checkbox controls the state -->
               <input type="checkbox" class="theme-controller" value="bumblebee" />
@@ -121,7 +121,7 @@ function playAudioj4(country) {
                   d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
               </svg>
             </label>
-          </details>
+          
         </li>
         <li>
           <details>
